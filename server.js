a3c2e4ac9858bcb777fc1944a44bd1048777b354
muhannad007 <muhannadafeefe654@gmail.com>
2024-11-26 12:34:07 +0300

    openai

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const functions = require("firebase-functions");
+const cors = require("cors");
 const router = require("./routers/Router");
 const app = express();
 
@@ -6,6 +8,9 @@ app.listen(3000, () => {
   console.log("Listening to port: ", 3000);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
+
+exports.app = functions.https.onRequest(app);
