06cd7696d7bf78535d8678158f1d124395553126
muhannad007 <muhannadafeefe654@gmail.com>
2024-11-28 00:43:54 +0300

    openai

--- server.js
+++ server.js
@@ -16,9 +16,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", router);
 
 exports.CvGeneration = onCall(
   { enforceAppCheck: true, secrets: [openAiDevKeySecret] },
-  app
+  app.use("/", router)
 );
